012db12c26feb83d09e301554f2ca1b20ed5226b
largeTree <1162796928@qq.com>
2018-08-26 00:06:37 +0800

    去除多余注解

--- cute-framework-web/src/main/java/com/qiuxs/cuteframework/web/AbstractServletInitializer.java
+++ cute-framework-web/src/main/java/com/qiuxs/cuteframework/web/AbstractServletInitializer.java
@@ -3,7 +3,6 @@ package com.qiuxs.cuteframework.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -14,9 +13,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ServletComponentScan("com.qiuxs")
 @ComponentScan(basePackages = { "com.qiuxs" })
 @MapperScan(basePackages = { "com.qiuxs.**.dao" })
-@SpringBootApplication
 @EnableAutoConfiguration
-public class AbstractServletInitializer extends SpringBootServletInitializer {
+public abstract class AbstractServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
