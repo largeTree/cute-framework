4ff2ef8e48873a28150b4b7f386c8217fc88e713
qiuxs <qiuxs@qiuxs.com>
2019-03-11 20:21:56 +0800

    简化

--- cute-framework-core/src/main/java/com/qiuxs/cuteframework/tech/mybatis/interceptor/utils/MbiHookUtils.java
+++ cute-framework-core/src/main/java/com/qiuxs/cuteframework/tech/mybatis/interceptor/utils/MbiHookUtils.java
@@ -1,19 +1,17 @@
 package com.qiuxs.cuteframework.tech.mybatis.interceptor.utils;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-
-import org.springframework.core.Ordered;
 
 import com.qiuxs.cuteframework.core.context.ApplicationContextHolder;
 import com.qiuxs.cuteframework.tech.mybatis.interceptor.hook.IMbiHook;
 
 /**
  * MyBatis钩子工具
+ * 
  * @author qiuxs
  * 
- * 创建时间 ： 2018年8月17日 下午8:36:54
+ *         创建时间 ： 2018年8月17日 下午8:36:54
  *
  */
 public class MbiHookUtils {
@@ -22,23 +20,20 @@ public class MbiHookUtils {
 
 	/**
 	 * 获取所有MyBatis钩子
+	 * 
 	 * @author qiuxs
 	 *
 	 * @return
 	 *
-	 * 创建时间：2018年8月17日 下午8:37:07
+	 * 		创建时间：2018年8月17日 下午8:37:07
 	 */
 	public static List<IMbiHook> getMbiHooks() {
 		if (mbiHooks == null) {
 			synchronized (MbiHookUtils.class) {
 				if (mbiHooks == null) {
 					mbiHooks = ApplicationContextHolder.getBeansForType(IMbiHook.class);
-					Collections.sort(mbiHooks, new Comparator<Ordered>() {
-						@Override
-						public int compare(Ordered o1, Ordered o2) {
-							return o1.getOrder() > o2.getOrder() ? 1 : o1.getOrder() == o2.getOrder() ? 0 : -1;
-						}
-					});
+					Collections.sort(mbiHooks,
+							(o1, o2) -> o1.getOrder() > o2.getOrder() ? 1 : o1.getOrder() == o2.getOrder() ? 0 : -1);
 				}
 			}
 		}
