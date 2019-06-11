a8c202fbdcad6972a6f64ca5267a2307149319f6
qiuxs <qiuxs@qiuxs.com>
2019-06-11 22:46:07 +0800

    完善sql

--- cute-framework-web/initSql/init.sql
+++ cute-framework-web/initSql/init.sql
@@ -8,6 +8,8 @@ CREATE TABLE `api_request_log`(
 	`req_end_time` DATETIME(3) NOT NULL COMMENT'请求结束时间'
 );
 CREATE INDEX `idx_request_log_ApiKey` ON api_request_log(`api_key`);
-
-
 ALTER TABLE `api_request_log` ADD COLUMN `status` TINYINT(3) NOT NULL DEFAULT 1 COMMENT'请求状态';
+
+-- 创建视图，方便查询数据
+CREATE ALGORITHM = UNDEFINED VIEW v_api_request_log AS 
+SELECT *,ROUND(UNIX_TIMESTAMP(req_end_time) * 1000 - UNIX_TIMESTAMP(req_start_time) * 1000, 0) AS cost_ms FROM api_request_log;
