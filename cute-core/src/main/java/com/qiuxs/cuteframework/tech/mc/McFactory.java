69a4335bfba2c23324457a62194b6445c44267a0
largeTree <qiuxs@qiuxs.com>
2020-01-30 13:19:02 +0800

    会话信息支持缓存到redis redisMcFactory单元测试

--- cute-core/src/main/java/com/qiuxs/cuteframework/tech/mc/McFactory.java
+++ cute-core/src/main/java/com/qiuxs/cuteframework/tech/mc/McFactory.java
@@ -20,49 +20,67 @@ import com.qiuxs.cuteframework.tech.mc.redis.utils.RedisQueue;
 import com.qiuxs.cuteframework.tech.mc.redis.utils.RedisSet;
 
 /**
- * 根据配置创建需要的集合，可能是本地的或远程的。
- * 
- * @author zhangyz created on 2013-10-19
+ * 根据配置创建需要的集合，可能是本地的或远程的。.
+ *
+ * @author qiuxs created on 2013-10-19
  */
 public class McFactory {
-	/** 默认本地,防止出空指针 */
-	private static ServerType serverType = ServerType.local;
+	
+	/**  默认本地,防止出空指针. */
+	private static McServerType serverType = McServerType.local;
 
-	/** 锁缓存 */
+	/**  锁缓存. */
 	private Map<String, Lock> mapLock = new ConcurrentHashMap<String, Lock>();
 
-	/** 缓存对象名set,防止单服务器内部重名 */
+	/**  缓存对象名set,防止单服务器内部重名. */
 	private Set<String> mcName = new HashSet<String>();
 
+	/** The db index. */
 	private int dbIndex = 0;
 
 	/**
 	 * 分布式集合类型枚举<br>
-	 * 2016-05-26 JinXinhua seq(序列)只实现了redis
-	 * 
+	 * 2016-05-26 JinXinhua seq(序列)只实现了redis.
+	 *
 	 * @author zhangyz created on 2013-5-9
 	 * @since Framework 1.0
 	 */
 	public enum McType {
-		map, queue, list, set, lock, seq
+
+		/** The map. */
+		map,
+		/** The queue. */
+		queue,
+		/** The list. */
+		list,
+		/** The set. */
+		set,
+		/** The lock. */
+		lock,
+		/** The seq. */
+		seq
 	}
 
 	/**
 	 * 内存服务器提供商枚举类型 <br>
-	 * 2016-05-25 JinXinhua redis用redis sentinel重新实现(包在redis2)<br>
-	 * 2016-06-12 JinXinhua islocal配置统一成mc_type<br>
-	 * 
-	 * @author zhangyz created on 2013-12-19
+	 *
+	 * @author qiuxs created on 2013-12-19
 	 */
-	public enum ServerType {
-		redis, local
-	}
+	public enum McServerType {
+		/** The local. */
+		local,
 
+		/** The redis. */
+		redis
+	}
+	
 	/**
-	 * 检查有无重名；但分布式环境下多台服务器检查名字有无重复还无法做到！
-	 * 
-	 * @throws RuntimeException
+	 * 检查有无重名；但分布式环境下多台服务器检查名字有无重复还无法做到！.
+	 *
 	 * @author zhangyz created on 2013-5-9
+	 * @param mcType the mc type
+	 * @param name the name
+	 * @throws RuntimeException the runtime exception
 	 */
 	private synchronized void checkNameExist(McType mcType, String name) throws RuntimeException {
 		String fullName = mcType.toString() + "/" + name;
@@ -73,23 +91,20 @@ public class McFactory {
 	}
 
 	/**
-	 * 使用分布式内存情况下，需要在调用之前设置一下参数，只需在系统启动时设置一次即可。
-	 * 
-	 * @param srvType
-	 *            使用的服务类型
-	 * @param className
-	 *            相应的配置文件(类路径)，一般每种服务都有自己的配置文件,若为null的话使用默认配置
+	 * 使用分布式内存情况下，需要在调用之前设置一下参数，只需在系统启动时设置一次即可。.
+	 *
 	 * @author zhangyz created on 2013-12-19
+	 * @param srvType            使用的服务类型
 	 */
-	public static void init(ServerType srvType, String className) {
+	public static void init(McServerType srvType) {
 		serverType = srvType;
 	}
 
 	/**
-	 * 单例模式，获取map工厂。初始化配置
-	 * 
-	 * @return
+	 * 单例模式，获取map工厂。初始化配置.
+	 *
 	 * @author zhangyz created on 2013-3-9
+	 * @return the factory
 	 */
 	public static McFactory getFactory() {
 		return McFactoryHolder.FACTORY;
@@ -105,26 +120,39 @@ public class McFactory {
 	 * @version 1.0.0
 	 */
 	private static class McFactoryHolder {
+		
+		/** The Constant FACTORY. */
 		static final McFactory FACTORY = new McFactory();
 	}
 
 	/**
-	 * 创建一个map,采用指定的名字
-	 * 
-	 * @param mapId
-	 *            指定的名字
-	 * @param keyClass
-	 *            key的类名
-	 * @param valueClass
-	 *            value的类名
-	 * 
-	 * @return
+	 * 创建一个map,采用指定的名字.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param keyClass            key的类名
+	 * @param valueClass            value的类名
+	 * @param initSize the init size
+	 * @param mapId            指定的名字
+	 * @return the map< k, v>
 	 */
 	public <K, V> Map<K, V> createMap(Class<K> keyClass, Class<V> valueClass,
 	        int initSize, String mapId) {
 		return createMap(keyClass, valueClass, initSize, mapId, true);
 	}
 
+	/**
+	 * Creates a new Mc object.
+	 *
+	 * @param <K> the key type
+	 * @param <V> the value type
+	 * @param keyClass the key class
+	 * @param valueClass the value class
+	 * @param initSize the init size
+	 * @param mapId the map id
+	 * @param checkName the check name
+	 * @return the map< k, v>
+	 */
 	public <K, V> Map<K, V> createMap(Class<K> keyClass, Class<V> valueClass,
 	        int initSize, String mapId, boolean checkName) {
 		if (checkName) {
@@ -147,10 +175,27 @@ public class McFactory {
 		return map;
 	}
 
+	/**
+	 * Creates a new Mc object.
+	 *
+	 * @param <V> the value type
+	 * @param valueClass the value class
+	 * @param setId the set id
+	 * @return the set< v>
+	 */
 	public <V> Set<V> createSet(Class<V> valueClass, String setId) {
 		return createSet(valueClass, setId, true);
 	}
 
+	/**
+	 * Creates a new Mc object.
+	 *
+	 * @param <V> the value type
+	 * @param valueClass the value class
+	 * @param setId the set id
+	 * @param checkName the check name
+	 * @return the set< v>
+	 */
 	public <V> Set<V> createSet(Class<V> valueClass, String setId, boolean checkName) {
 		if (checkName) {
 			checkNameExist(McType.set, setId);
@@ -170,23 +215,26 @@ public class McFactory {
 	}
 
 	/**
-	 * 创建一个固定的List
-	 * 
-	 * @param listId
-	 *            列表标识，该列表会被永久启用,用于分布式下场景
-	 * @return
+	 * 创建一个固定的List.
+	 *
+	 * @param <V> the value type
+	 * @param valueClass the value class
+	 * @param listId            列表标识，该列表会被永久启用,用于分布式下场景
+	 * @return the list< v>
 	 */
 	public <V> List<V> createList(Class<V> valueClass, String listId) {
 		return createList(valueClass, listId, true);
 	}
 
 	/**
-	 * 创建一个固定的List
-	 * 
-	 * @param listId
-	 *            列表标识，该列表会被永久启用,用于分布式下场景
-	 * @return
+	 * 创建一个固定的List.
+	 *
 	 * @author zhangyz created on 2013-3-10
+	 * @param <V> the value type
+	 * @param valueClass the value class
+	 * @param listId            列表标识，该列表会被永久启用,用于分布式下场景
+	 * @param checkName the check name
+	 * @return the list< v>
 	 */
 	public <V> List<V> createList(Class<V> valueClass, String listId, boolean checkName) {
 		if (checkName) {
@@ -208,14 +256,13 @@ public class McFactory {
 	}
 
 	/**
-	 * 创建一个固定的queue
-	 * 
-	 * @param valueClass
-	 *            list中元素类名
-	 * @param listId
-	 *            列表标识，该列表会被永久启用,用于分布式下场景
-	 * @return
+	 * 创建一个固定的queue.
+	 *
 	 * @author zhangyz created on 2013-3-10
+	 * @param <V> the value type
+	 * @param valueClass            list中元素类名
+	 * @param queueId the queue id
+	 * @return the blocking queue< v>
 	 */
 	public <V> BlockingQueue<V> createQueue(Class<V> valueClass, String queueId) {
 		checkNameExist(McType.queue, queueId);
@@ -235,11 +282,11 @@ public class McFactory {
 	}
 
 	/**
-	 * 为了避免客户端锁对象的创建资源浪费
-	 * 
-	 * @param lockName
-	 * @return
+	 * 为了避免客户端锁对象的创建资源浪费.
+	 *
 	 * @author zhangyz created on 2013-4-26
+	 * @param lockName the lock name
+	 * @return the lock
 	 */
 	public Lock getLock(String lockName) {
 		Lock lock = mapLock.get(lockName);
@@ -250,6 +297,12 @@ public class McFactory {
 		return lock;
 	}
 
+	/**
+	 * Creates a new Mc object.
+	 *
+	 * @param lockName the lock name
+	 * @return the lock
+	 */
 	private Lock createLock(String lockName) {
 		Lock ret = null;
 		switch (serverType) {
@@ -266,12 +319,17 @@ public class McFactory {
 	}
 
 	/**
-	 * 出现异常清除缓存名称重新加载
+	 * 出现异常清除缓存名称重新加载.
 	 */
 	public void clearMcName() {
 		mcName.clear();
 	}
 
+	/**
+	 * Gets the 缓存对象名set,防止单服务器内部重名.
+	 *
+	 * @return the 缓存对象名set,防止单服务器内部重名
+	 */
 	public Set<String> getMcName() {
 		return mcName;
 	}
---
+++ cute-core/src/test/java/com/qiuxs/cuteframework/mc/McFactoryTester.java
@@ -0,0 +1,36 @@
+package com.qiuxs.cuteframework.mc;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.qiuxs.cuteframework.core.basic.utils.generator.RandomGenerator;
+import com.qiuxs.cuteframework.core.persistent.redis.RedisConfiguration;
+import com.qiuxs.cuteframework.tech.mc.McFactory;
+import com.qiuxs.cuteframework.tech.mc.McFactory.McServerType;
+
+public class McFactoryTester {
+
+	
+	@Before
+	public void setUp() {
+		new RedisConfiguration().initDefaultPoll();
+	}
+	
+	@Test
+	public void testRedisMap() {
+		McFactory.init(McServerType.redis);
+		Map<String, String> map = McFactory.getFactory().createMap(String.class, String.class, 10, "test_string_map");
+		String val = RandomGenerator.getRandomStringByLength(10);
+		map.put("key", val);
+
+		McFactory.getFactory().clearMcName();
+
+		Map<String, String> map2 = McFactory.getFactory().createMap(String.class, String.class, 10, "test_string_map");
+		String val2 = map2.get("key");
+		Assert.assertEquals("Assert redis Val", val, val2);
+	}
+	
+}
