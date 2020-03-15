6c32551b34599058ac3af3960144c92f0e0ff9f5
largeTree <qiuxs@qiuxs.com>
2020-03-15 12:12:15 +0800

    增加日志

--- cute-core/src/main/java/com/qiuxs/cuteframework/tech/spring/config/CutePropertyPlaceholderConfigurer.java
+++ cute-core/src/main/java/com/qiuxs/cuteframework/tech/spring/config/CutePropertyPlaceholderConfigurer.java
@@ -3,6 +3,8 @@ package com.qiuxs.cuteframework.tech.spring.config;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 
 import com.qiuxs.cuteframework.core.basic.config.IConfiguration;
@@ -15,6 +17,8 @@ import com.qiuxs.cuteframework.core.basic.config.UConfigUtils;
  */
 public class CutePropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
 
+	private static Logger log = LogManager.getLogger(CutePropertyPlaceholderConfigurer.class);
+	
 	private Properties ctxProperties = new Properties();
 
 	@Override
@@ -23,6 +27,7 @@ public class CutePropertyPlaceholderConfigurer extends PropertyPlaceholderConfig
 		IConfiguration domain = UConfigUtils.getDomain(UConfigUtils.DOMAIN_DB);
 		props.putAll(domain.toMap());
 		ctxProperties.putAll(props);
+		log.info("db.driver = " + props.getProperty("db.driver") + ", db.url = " + props.getProperty("db.url") + ", db.user = " + props.getProperty("db.user"));
 	}
 
 	/***
