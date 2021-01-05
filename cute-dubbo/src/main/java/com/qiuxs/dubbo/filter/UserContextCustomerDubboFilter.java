c5edaab14cc68da78fbfee7d6b8d6618b64e4298
qiuxs <qiuxs@qiuxs.com>
2021-01-05 11:24:33 +0800

    增加filter

---
+++ cute-dubbo/src/main/java/com/qiuxs/dubbo/filter/UserContextCustomerDubboFilter.java
@@ -0,0 +1,16 @@
+package com.qiuxs.dubbo.filter;
+
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+
+public class UserContextCustomerDubboFilter implements Filter {
+
+	@Override
+	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+		return invoker.invoke(invocation);
+	}
+
+}
